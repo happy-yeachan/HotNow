cdff686100f6c20a7cd146e7dc8e446b7a775f1d
happy-yeachan <dus001228@naver.com>
2025-08-05 01:25:00 +0900

    feat: 트렌드, 알람 라우팅 추가

--- hotnow/urls.py
+++ hotnow/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('auths.urls')),
     path('api/v1/users/', include('users.urls')),
+    path('api/v1/trends/', include('trends.urls')),
+    path('api/v1/alerts/', include('alerts.urls')),
 ]
