8f5d79cdc780baa9a171484cef16018dd052a491
happy-yeachan <dus001228@naver.com>
2025-08-04 15:41:38 +0900

    feat: Auth serializers 구현 - 비밀번호 보안 강화

---
+++ auths/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from users.models import User
+from django.contrib.auth.hashers import make_password
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'password', 'nickname']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
