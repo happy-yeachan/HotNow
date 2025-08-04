15510b9087156ebb3aa5a0b6ef1e9cd4e4a4e1d4
happy-yeachan <dus001228@naver.com>
2025-08-04 13:41:46 +0900

    feat: User모델 커스텀 - 위치 속성 추가 - email 기반 로그인

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=100)
+    profile_image_url = models.TextField(blank=True, null=True)
+    location_lat = models.DecimalField(max_digits=10, decimal_places=7, blank=True, null=True)
+    location_lng = models.DecimalField(max_digits=10, decimal_places=7, blank=True, null=True)
+    location_name = models.CharField(max_length=255, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'               # 로그인 ID로 email 사용
+    REQUIRED_FIELDS = ['username']         # createsuperuser 명령에서 필요
+
+    def __str__(self):
+        return self.email
